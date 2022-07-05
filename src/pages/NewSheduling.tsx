774af80115bfbef317cbf6b0d13c601ca3e993ae
Pedro S. Pereira <pedros.pereira558@gmail.com>
2022-07-05 10:12:50 -0300

    update styling

--- src/pages/NewSheduling.tsx
+++ src/pages/NewSheduling.tsx
@@ -1,4 +1,4 @@
-import { Plus, ArrowLeft } from "phosphor-react";
+import { Plus, ArrowLeft, House } from "phosphor-react";
 
 interface DataTask {
     id: number;
@@ -22,8 +22,8 @@ export function NewSheduling() {
                     alt="Logo Bickup"
                     className="w-16 fixed  top-[1.28rem] left-[1rem]"
                 />
-                <div className="w-[3.62rem] h-[3.62rem] bg-white rounded-2xl flex  items-center justify-center fixed top-[(50vh)_-_25%] hover:opacity-90 cursor-pointer transition">
-                    <Plus color="#ffb715" size={20} weight="bold" />
+                <div className="w-[3.62rem] h-[3.62rem] bg-white rounded-full flex  items-center justify-center fixed top-[(50vh)_-_25%] hover:scale-110 cursor-pointer transition ">
+                <House size={32} color="#FFB715" weight="light" />
                 </div>
             </header>
 
